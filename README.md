a85c67366fff521d3ea57f776f054f9a3860d0ca
Hannah <hannahjahn1024@gmail.com>
2023-07-25 18:04:54 -0700

    in progress

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 #Questions to ask
-## 1. How to get .env into our fetch URL
-## 2. Why is our API only giving 3 days forecast instead of 7
+## 1. useState functions are not running at refresh
--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,37 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
-  
-  Promise.all([
-    fetch('http://api.weatherapi.com/v1/current.json?key=MY_KEY&q=Los Angeles'),
-    fetch('http://api.weatherapi.com/v1/forecast.json?key=MY_KEY&q=Los Angeles&days=7')
-  ]).then(function (responses) {
-    return Promise.all(responses.map(function (response) {
-      return response.json();
-    }))
-  }).then(function (data) {
-    console.log(data);
-  });
+  const [defaultWeather, setDefaultWeather] = useState(null);
+  const [searched, setSearched] = useState(null);
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_REACT_APP_MY_KEY}&q=Los Angeles`),
+      fetch(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_REACT_APP_MY_KEY}&q=Los Angeles&days=7`)
+    ]).then(function (responses) {
+      return Promise.all(responses.map(function (response) {
+        return response.json();
+      }))
+    }).then(function (data) {
+      console.log(data);
+      setDefaultWeather(data[0]);
+      setSearched(data[1]);
+      console.log(defaultWeather);
+      console.log(searched);
+    });
+  }, []);
 
 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => console.log(`${import.meta.env.REACT_APP_MY_KEY}`)}>
-          count is {count}
+      <div className="box">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => console.log(`${import.meta.env.REACT_APP_MY_KEY}`)}>
+          count
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
